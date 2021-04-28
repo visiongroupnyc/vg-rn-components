ebc34387eb62c4fe910069e7e35ef31a3bbd2fa1
Mauro Luna <mauroluna.dev@gmail.com>
2021-04-28 15:09:30 -0300

    fix(Text): add Text into export file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import Header from './Header/header.component';
 import Screen from './Screen/screen.component';
+import Text from './Text/text.component';
 
 export {
   Header,
   Screen,
+  Text,
 };
